cf4daf3a98a2661c3c21176bf30cb20a2af7328c
Bryce Rigler <b-rigler@hotmail.com>
2023-04-10 00:46:39 -0600

    Update canvas.js

--- assignment-11 (from example)/scripts/canvas.js
+++ assignment-11 (from example)/scripts/canvas.js
@@ -97,7 +97,31 @@ function getKey(event)
     }
     drawSquare(); 
 } 
-
+if(test2)
+    {
+        lives++;
+   squareArray[0].x-=600;
+   
+        if(direction == "left")
+        {
+            moveRight();
+        }
+        else if(direction == "right")
+        {
+            moveLeft();
+        }
+        else if(direction == "up")
+        {
+            moveDown();
+        }
+        else if(direction == "down")
+        {
+            moveUp();
+        }
+    
+    }
+    drawSquare(); 
+} 
 
 function moveUp()
 {
