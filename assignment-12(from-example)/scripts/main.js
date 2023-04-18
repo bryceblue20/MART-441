a8a95fa4533ead90b00ad6809efcf104357d6dfe
Bryce Rigler <b-rigler@hotmail.com>
2023-04-18 11:40:06 -0600

    Update main.js

--- assignment-12(from-example)/scripts/main.js
+++ assignment-12(from-example)/scripts/main.js
@@ -17,44 +17,68 @@ var config = {
         update: update
     }
 };
-let keyBar;
+
 var player;
 var stars;
 var bombs;
 var platforms;
 var cursors;
+var third;
+var second;
+var first;
+var expert;
+var master;
 var score = 0;
+var trophies = 0;
+var level = 1;
 var gameOver = false;
 var scoreText;
 
 var game = new Phaser.Game(config);
 
+
 function preload() {
 
-    this.load.image('ground', '../assets/platform.png');
-    this.load.image('star', '../assets/star.png');
-    this.load.image('bomb', '../assets/bomb.png');
-    this.load.spritesheet('dude', '../assets/dude.png', {
+    this.load.image('natureworld','bg.png');
+    this.load.image('ground', 'platform.png');
+    this.load.image('floor', 'groundlayer.png');
+    this.load.image('star', 'gemBlue.png');
+    this.load.image('bomb', 'spinner_spin.png');
+    this.load.image('bronze', 'bronze.png');
+    this.load.image('rookiebadge', 'rookiebadge.png');
+    this.load.image('silver', 'silver.png');
+    this.load.image('traineebadge', 'traineebadge.png');
+    this.load.image('gold', 'gold.png');
+    this.load.image('veteranbadge', 'veteranbadge.png');
+    this.load.image('platinum', 'platinum.png');
+    this.load.image('expertbadge', 'expertbadge.png');
+    this.load.image('diamond', 'diamond.png');
+    this.load.image('masterbadge', 'masterbadge.png');
+    this.load.spritesheet('dude', '/assets/dude.png', {
         frameWidth: 32,
         frameHeight: 48
     });
 }
 
 function create() {
-    //  A simple background for our game
-  keyBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    
 
+    //  A simple background for our game
+    this.add.image(400, 300, 'natureworld');
+    
+   
     //  The platforms group contains the ground and the 2 ledges we can jump on
+
     platforms = this.physics.add.staticGroup();
 
     //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite  is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    platforms.create(400, 568, 'floor').setScale(2).refreshBody();
 
     //  Now let's create some ledges
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+    platforms.create(400, 300, 'ground');
+    platforms.create(750, 440, 'ground');
+    platforms.create(50, 440, 'ground');
 
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'dude');
@@ -63,6 +87,8 @@ function create() {
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+    
+  
     //  Our player animations, turning, walking left and walking right.
     this.anims.create({
         key: 'left',
@@ -99,11 +125,11 @@ function create() {
     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
     stars = this.physics.add.group({
         key: 'star',
-        repeat: 11,
+        repeat: 9,
         setXY: {
-            x: 12,
+            x: 10,
             y: 0,
-            stepX: 70
+            stepX: 80
         }
     });
 
@@ -115,22 +141,34 @@ function create() {
     });
 
     bombs = this.physics.add.group();
+    
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', {
-        fontSize: '32px',
-        fill: '#000'
+        fontSize: '40px',
+        fill: '#000000'
     });
 
+trophyText = this.add.text(16,50, 'Class: Beginner :3', {
+    fontSize: '32px',
+    fill: '#000000'
+});
+
+levelText = this.add.text(600,16, 'Level: 1', {
+    fontSize: '32px',
+    fill: '#FFFFFF'
+});
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
+  
 
     //  Checks to see if the player overlaps with any of the stars if he does call the collectStar function
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
     this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 }
 
 function update() {
@@ -139,11 +177,11 @@ function update() {
     }
 
     if (cursors.left.isDown) {
-        player.setVelocityX(-160);
+        player.setVelocityX(-200);
 
         player.anims.play('left', true);
     } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
+        player.setVelocityX(200);
 
         player.anims.play('right', true);
     } else {
@@ -152,8 +190,8 @@ function update() {
         player.anims.play('turn');
     }
 
-    if (keyBar.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-310);
     }
 }
 
@@ -164,7 +202,57 @@ function collectStar(player, star) {
     score += 10;
     scoreText.setText('Score: ' + score);
 
+    if (score=='300') {
+        third = this.physics.add.image(250, 200, 'bronze');
+        third.setBounce(0.8);
+        third.setCollideWorldBounds(true);;
+        this.physics.add.collider(third, platforms);
+        this.physics.add.collider(player, third, collectBronze, null, this);
+        trophyText.setText('collect your trophy!');
+    }
+    if (score=='400') {
+        second = this.physics.add.image(280, 200, 'silver');
+        second.setBounce(0.8);
+        second.setCollideWorldBounds(true);;
+        this.physics.add.collider(second, platforms);
+        this.physics.add.collider(player, second, collectSilver, null, this);
+
+    }
+
+    if (score=='550') {
+        first = this.physics.add.image(300, 200, 'gold');
+        first.setBounce(0.8);
+        first.setCollideWorldBounds(true);;
+        this.physics.add.collider(first, platforms);
+        this.physics.add.collider(player, first, collectGold, null, this);
+
+    }
+
+    if (score=='650') {
+        expert = this.physics.add.image(310, 200, 'platinum');
+        expert.setBounce(0.4);
+        expert.setCollideWorldBounds(true);;
+        this.physics.add.collider(expert, platforms);
+        this.physics.add.collider(player, expert, collectPlatinum, null, this);
+        
+    }
+
+    if (score=='800') {
+        master = this.physics.add.image(420, 200, 'diamond');
+        master.setBounce(0.4);
+        master.setCollideWorldBounds(true);;
+        this.physics.add.collider(master, platforms);
+        this.physics.add.collider(player, master, collectDiamond, null, this);
+        
+
+    }
+
+
+
     if (stars.countActive(true) === 0) {
+    level += 1
+    levelText.setText ('Level: ' + level)
+    
         //  A new batch of stars to collect
         stars.children.iterate(function (child) {
 
@@ -177,7 +265,7 @@ function collectStar(player, star) {
         var bomb = bombs.create(x, 16, 'bomb');
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.setVelocity(Phaser.Math.Between(-250, 250), 20);
         bomb.allowGravity = false;
 
     }
@@ -191,4 +279,42 @@ function hitBomb(player, bomb) {
     player.anims.play('turn');
 
     gameOver = true;
+}
+
+function collectBronze(player, third) {
+    
+    trophyText.setText('Bronze/ Class: Rookie');
+    fill: '#CD7F32'
+    third.disableBody(true, true);
+    this.add.image(120, 440, 'rookiebadge');
+
+    
+}
+
+function collectSilver(player, second) {
+   
+    trophyText.setText('Silver/ Class: Trainee');
+    second.disableBody(true, true);
+    this.add.image(680, 440, 'traineebadge');
+}
+
+function collectGold(player, first) {
+   
+    trophyText.setText('Gold/ Class: Veteran');
+    first.disableBody(true, true);
+    this.add.image(400, 300, 'veteranbadge');
+}
+
+function collectPlatinum(player, expert) {
+   
+    trophyText.setText('Platinum/ Class: Expert!');
+    expert.disableBody(true, true);
+    this.add.image(400, 560, 'expertbadge');
+}
+
+function collectDiamond(player, master) {
+   
+    trophyText.setText('Class: Master of Gems!');
+    master.disableBody(true, true);
+    this.add.image(400, 150, 'masterbadge');
 }
